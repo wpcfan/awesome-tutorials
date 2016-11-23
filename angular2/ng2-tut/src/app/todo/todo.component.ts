72135145a58542d4c262f280a6bb6ada638f41be
Peng Wang <peng.wang@outlook.com>
2016-11-23 15:51:59 +0800

    async service method and todo app styling

--- angular2/ng2-tut/src/app/todo/todo.component.ts
+++ angular2/ng2-tut/src/app/todo/todo.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from './todo.model';
 import { TodoService } from './todo.service';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-  providers:[TodoService]
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-  todos: Todo[] = [];
+  todos : Todo[];
   desc = '';
-  constructor(private service:TodoService) { }
 
+  constructor(private service: TodoService) {}
   ngOnInit() {
+    this.getTodos();
   }
-
   addTodo(){
     this.service
       .addTodo(this.desc)
@@ -23,5 +23,33 @@ export class TodoComponent implements OnInit {
         this.todos = [...this.todos, todo];
         this.desc = '';
       });
+  }
+  toggleTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .toggleTodo(todo)
+      .then(t => {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          t,
+          ...this.todos.slice(i+1)
+          ];
+      });
+  }
+  removeTodo(todo: Todo) {
+    const i = this.todos.indexOf(todo);
+    this.service
+      .deleteTodoById(todo.id)
+      .then(()=> {
+        this.todos = [
+          ...this.todos.slice(0,i),
+          ...this.todos.slice(i+1)
+        ];
+      });
+  }
+  getTodos(): void {
+    this.service
+      .getTodos()
+      .then(todos => this.todos = [...todos]);
   }
 }
--- angular2/ng2-tut/src/app/todo/todo.service.ts
+++ angular2/ng2-tut/src/app/todo/todo.service.ts
@@ -11,9 +11,7 @@ export class TodoService {
 
   private api_url = 'api/todos';
   private headers = new Headers({'Content-Type': 'application/json'});
-
   constructor(private http: Http) { }
-
   // POST /todos
   addTodo(desc:string): Promise<Todo> {
     let todo = {
@@ -27,7 +25,33 @@ export class TodoService {
             .then(res => res.json().data as Todo)
             .catch(this.handleError);
   }
-
+  // PUT /todos/:id
+  toggleTodo(todo: Todo): Promise<Todo> {
+    const url = `${this.api_url}/${todo.id}`;
+    console.log(url);
+    let updatedTodo = Object.assign({}, todo, {completed: !todo.completed});
+    return this.http
+            .put(url, JSON.stringify(updatedTodo), {headers: this.headers})
+            .toPromise()
+            .then(() => updatedTodo)
+            .catch(this.handleError);
+  }
+  // DELETE /todos/:id
+  deleteTodoById(id: string): Promise<void> {
+    const url = `${this.api_url}/${id}`;
+    return this.http
+            .delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+  }
+  // GET /todos
+  getTodos(): Promise<Todo[]>{
+    return this.http.get(this.api_url)
+              .toPromise()
+              .then(res => res.json().data as Todo[])
+              .catch(this.handleError);
+  }
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
