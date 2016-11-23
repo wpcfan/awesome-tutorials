92425af2ecea7ce868cbdd89ab1b79bd87855a49
Peng Wang <peng.wang@outlook.com>
2016-11-23 15:31:09 +0800

    simple todo input and display

--- angular2/ng2-tut/src/app/todo/todo.component.ts
+++ angular2/ng2-tut/src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
+  todos: Todo[] = [];
+  desc = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTodo(){
+    this.todos.push({id: 1, desc: this.desc, completed: false});
+    this.desc = '';
+  }
 }
---
+++ angular2/ng2-tut/src/app/todo/todo.model.ts
@@ -0,0 +1,5 @@
+export class Todo {
+  id: number;
+  desc: string;
+  completed: boolean;
+}
