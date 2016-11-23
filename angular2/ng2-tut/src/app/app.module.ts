79ecd9afb2b797683ab3cd316626cd92a44dad5b
Peng Wang <peng.wang@outlook.com>
2016-11-23 15:11:35 +0800

    add routing to the app

--- angular2/ng2-tut/src/app/app.module.ts
+++ angular2/ng2-tut/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,7 +16,13 @@ import { AuthService } from './core/auth.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [
     {provide: 'auth',  useClass: AuthService}
